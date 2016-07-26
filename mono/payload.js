4b1f5e638247d837909d51a72b371b6ff90354a2
TheCjw <thecjw@live.com>
2016-07-26 16:16:32 +0800

    First commit.

---
+++ mono/payload.js
@@ -0,0 +1,64 @@
+(function() {
+    "use strict";
+
+    // Copy from https://stackoverflow.com/questions/951021/what-do-i-do-if-i-want-a-javascript-version-of-sleep
+    function sleep(time) {
+        return new Promise((resolve) => setTimeout(resolve, time));
+    }
+
+    // Waiting for libmono loading.
+
+    var libmono_base = null;
+    while (true) {
+        sleep(100);
+        libmono_base = Module.findBaseAddress("libmono.so");
+        if (libmono_base != null) {
+            break;
+        }
+    }
+
+    console.log("libmono base address: " + libmono_base);
+
+    // https://github.com/mono/mono/blob/0e97e079fa6f90c2bf864d315a2e2c8bf8ca7410/mono/metadata/image.c#L1272
+    // mono_image_open_from_data_internal (char *data,
+    // guint32 data_len,
+    // gboolean need_copy,
+    // MonoImageOpenStatus *status,
+    // gboolean refonly,
+    // gboolean metadata_only,
+    // const char *name)
+    var func_mono_image_open_from_data_with_name = new NativeFunction(
+        Module.findExportByName("libmono.so", "mono_image_open_from_data_with_name"), "pointer", ["pointer", "uint32", "uint32", "pointer", "uint32", "uint32", "pointer"]);
+    console.log("mono_image_open_from_data_with_name: " + func_mono_image_open_from_data_with_name);
+
+    try {
+        Interceptor.attach(func_mono_image_open_from_data_with_name, {
+            onEnter: function(args) {
+                try {
+                    let ptr_assembly_name = Memory.readUtf8String(args[5]);
+                    if (ptr_assembly_name.indexOf("Assembly-CSharp.dll") != -1) {
+                        this.data = args[0];
+                        this.size = args[1];
+                        console.log(ptr_assembly_name);
+                    }
+                } catch (e) {
+                    console.log(e);
+                }
+            },
+            onLeave: function(retval) {
+                try {
+                    if (this.data != null) {
+                        console.log(this.data + " " + this.size);
+                        var out = new File("/sdcard/holyshit.dll", "wb");
+                        out.write(Memory.readByteArray(this.data, this.size.toInt32()));
+                        out.close();
+                    }
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+        });
+    } catch (e) {
+        console.log(e);
+    }
+})();
---
+++ mono/reload.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: TheCjw<thecjw@live.com>
+# Created on 2016.05.12
+
+__author__ = "TheCjw"
+
+import os
+import time
+import sys
+
+import frida
+
+
+def on_message(message, data):
+    # print dir(message)
+    # print(data)
+    if message.has_key("payload"):
+        print message["payload"]
+
+
+def get_process_pid(device, package_name):
+    for p in device.enumerate_processes():
+        if p.name == package_name:
+            return p.pid
+    return -1
+
+
+def main():
+    device = frida.get_device_manager().enumerate_devices()[-1]
+    package_name = "com.skymoons.hqg.uc"
+
+    # find and kill process.
+    pid = get_process_pid(device, package_name)
+    if pid != -1:
+        print("killing {0}".format(pid))
+        device.kill(pid)
+        time.sleep(0.3)
+
+    os.system("adb shell am start -n {0}/{1}".format(package_name, "com.onektower.sdk.tgame.SplashActivity"))
+    time.sleep(0.2)
+
+    pid = get_process_pid(device, package_name)
+    if pid == -1:
+        print("{0} is not found...".format(package_name))
+        return
+
+    session = None
+    try:
+        session = frida.get_device_manager().enumerate_devices()[-1].attach(pid)
+        script_content = open("payload.js").read()
+        script = session.create_script(script_content)
+        script.on("message", on_message)
+        script.load()
+        sys.stdin.read()
+    except KeyboardInterrupt as e:
+        if session is not None:
+            session.detach()
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
