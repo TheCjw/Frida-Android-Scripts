90bb6f9c7cfeb15f10620d27efa347d0b1f52b95
TheCjw <thecjw@live.com>
2017-02-20 17:11:48 +0800

    Update loader.py

--- loader.py
+++ loader.py
@@ -11,120 +11,173 @@ import platform
 import subprocess
 import sys
 import time
+from datetime import datetime
 
 import frida
 
+import colorlog
+from colorlog import ColoredFormatter
 
-def script_suffix():
-    build_os = platform.system().lower()
-    return ".cmd" if build_os == 'windows' else ""
+handler = colorlog.StreamHandler()
 
+formatter = ColoredFormatter(
+    "%(log_color)s[%(asctime)s] [%(levelname)s]%(reset)s %(message)s",
+    datefmt="%H:%M:%S",
+    reset=True,
+    log_colors={
+        "DEBUG": "cyan",
+        "INFO": "green",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "red,bg_white",
+    }
+)
 
-def node_script_path(name):
-    return os.path.abspath(os.path.join(sys.path[0], "node_modules", ".bin", name + script_suffix()))
+handler.setFormatter(formatter)
 
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(help="commands")
-
-    cmd_run = subparsers.add_parser("run",
-                                    help="Restart package and inject Frida script.")
-    cmd_run.set_defaults(command="run")
-    cmd_run.add_argument("-p",
-                         "--package",
-                         type=str,
-                         help="Package name of Android app")
-    cmd_run.add_argument("-s",
-                         "--script",
-                         type=str,
-                         default="payload.js",
-                         help="Script to inject")
-    args = parser.parse_args()
-
-    return args
+logger = colorlog.getLogger("loader")
+logger.addHandler(handler)
+logger.level = 10  # DEBUG
 
 
-def on_message(message, data):
-    if isinstance(message, dict):
-        print(message)
-    else:
-        if message.has_key("payload"):
-            print(message["payload"])
+class MyArgParser(object):
+    def __init__(self):
 
+        self.frida_compile = self.__node_script_path__("frida-compile")
+        self.temp_script_path = os.path.join(os.path.dirname(__file__), "out")
 
-def get_application_name(device, identifier):
-    for p in device.enumerate_applications():
-        if p.identifier == identifier:
-            return p.name
+        parser = argparse.ArgumentParser(
+            description="A frida script loader for Android/iOS app",
+            usage="""{0} <command> [<args>]
 
+""".format(os.path.basename(__file__)))
 
-def get_process_pid(device, application_name):
-    for p in device.enumerate_processes():
-        if p.name == application_name:
-            return p.pid
-    return -1
+        parser.add_argument("command", help="Subcommand to run")
 
+        if len(sys.argv) == 1:
+            parser.print_help()
+            sys.exit(1)
 
-def main():
-    args = parse_args()
-    if args.command == "run":
-        script_file = args.script
-        package_name = args.package
+        args = parser.parse_args(sys.argv[1:2])
+        if not hasattr(self, args.command):
+            logger.error("Unrecognized command: {0}".format(args.command))
+            parser.print_help()
+            sys.exit(1)
 
-        if os.path.isabs(script_file) is False:
-            script_file = os.path.abspath(script_file)
+        getattr(self, args.command)()
 
-        # Using frida-compile convert ES6 script to ES5 script
-        print("[+] Compiling javascript files.")
-        out_path = os.path.join(os.path.dirname(__file__), "out")
-        generated_file = os.path.join(out_path, os.path.basename(script_file))
-        subprocess.check_call([node_script_path("frida-compile"), script_file, "-o", generated_file], cwd=os.getcwd())
-        
-        script_content = open(generated_file, encoding="utf-8").read()
+    @staticmethod
+    def __node_script_path__(name):
+        build_os = platform.system().lower()
+        suffix = ".cmd" if build_os == "windows" else ""
+        return os.path.abspath(os.path.join(sys.path[0], "node_modules",
+                                            ".bin", name + suffix))
+
+    @staticmethod
+    def __get_application_name__(device, identifier):
+        for p in device.enumerate_applications():
+            if p.identifier == identifier:
+                return p.name
+
+    @staticmethod
+    def __get_process_pid__(device, application_name):
+        for p in device.enumerate_processes():
+            if p.name == application_name:
+                return p.pid
+        return -1
+
+    def __compile_javascript__(self, src, dst):
+        """
+        Using frida-compile convert ES6 script to ES5 script
+        :param script_path:
+        :param output_path:
+        :return:
+        """
+        logger.info("Compiling {0} file with frida-compile.".format(os.path.basename(src)))
+        subprocess.check_call([self.frida_compile, src, "-o", dst],
+                              cwd=os.getcwd())
+
+    def __run_and_inject__(self, package_name, script_path):
+        print("[*] Staring at {0}".format(datetime.now().strftime("%H:%M:%S")))
+
+        if os.path.isabs(script_path) is False:
+            script_path = os.path.abspath(script_path)
+
+        output_script = os.path.join(self.temp_script_path, os.path.basename(script_path))
+        self.__compile_javascript__(script_path, output_script)
+
+        script_content = open(output_script, encoding="utf-8").read()
+
         # Update some consts here.
         script_content = script_content.replace("__PACKAGE_NAME__", package_name)
 
+        # FIXME: add iOS support sometime.
         device = frida.get_device_manager().enumerate_devices()[-1]
 
-        pid = get_process_pid(device, package_name)
-        if pid != -1:
-            print("[+] killing {0}".format(pid))
-            device.kill(pid)
-            # Pause for 3s.
-            time.sleep(0.3)
+        # Kill running package.
+        while True:
+            pid = self.__get_process_pid__(device, package_name)
+            if pid != -1:
+                logger.info("killing {0}".format(pid))
+                # os.system(r"adb shell am force-stop {0}".format(package))
+                os.system(r"adb shell su -c \"pgrep {0} | xargs kill\"".format(package_name))
+                time.sleep(0.1)
+            else:
+                break
 
-        os.system(r"adb shell su -c \"echo 4 > /data/data/{0}/files/.pause\"".format(package_name))
+        # TODO: Use PauseOnStart(Xposed Module) to suspend app at *attachBaseContext.
         os.system(r"adb shell monkey -p {0} -c android.intent.category.LAUNCHER 1".format(package_name))
 
         pid = -1
-        for i in range(10):
-            pid = get_process_pid(device, package_name)
+        for i in range(15):
+            pid = self.__get_process_pid__(device, package_name)
             if pid != -1:
                 break
-            time.sleep(0.1)
+            time.sleep(0.05)
 
         if pid == -1:
-            print("[-] Run package {0} failed.".format(package_name))
+            logger.error("Run package {0} failed.".format(package_name))
             return
 
-        print("[+] Injecting {0} to {1}({2})".format(script_file, package_name, pid))
+        logger.info("Injecting {0} to {1}({2})".format(os.path.basename(script_path),
+                                                       package_name, pid))
 
         session = None
         try:
             session = frida.get_device_manager().enumerate_devices()[-1].attach(pid)
-           
             script = session.create_script(script_content)
-            script.on("message", on_message)
             script.load()
             sys.stdin.read()
-        except KeyboardInterrupt as e:
+        except KeyboardInterrupt:
             if session is not None:
                 session.detach()
-            sys.exit(0)
+            logger.error("user aborted")
 
-    else:
-        pass
+        print("[*] Shutting down at {0}".format(datetime.now().strftime("%H:%M:%S")))
+
+    def run(self):
+        """
+        Spawn a new process with package name, then inject frida script.
+        :return:
+        """
+        parser = argparse.ArgumentParser(
+            description="Spawn a new process with package and inject script.")
+        parser.add_argument("-p",
+                            "--package",
+                            type=str,
+                            help="Package name of app",
+                            required=True)
+        parser.add_argument("-s",
+                            "--script",
+                            type=str,
+                            help="Script to inject",
+                            required=True)
+        args = parser.parse_args(sys.argv[2:])
+        self.__run_and_inject__(args.package, args.script)
+
+
+def main():
+    MyArgParser()
 
 
 if __name__ == "__main__":
